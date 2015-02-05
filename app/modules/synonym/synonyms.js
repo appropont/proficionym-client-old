c6cbdfecae3e1dd6b5386f0725fe62642d58ec75
Chris Griffing <cmgriffing@gmail.com>
2015-02-04 18:20:21 -0800

    added angular-xml and added it as an interceptor for services.synonyms

--- app/modules/synonym/synonyms.js
+++ app/modules/synonym/synonyms.js
@@ -1,32 +1,37 @@
 define(['angular', 'apiKeys', 'services.jsonip'], function (angular, apiKeys) {
-  'use strict';
-  angular.module('services.synonyms', [])
-    .factory('synonyms', function synonymsFactory($http, $q) {
+    'use strict';
+    angular.module('services.synonyms', [])
+	    .config(function ($httpProvider) {
+	     	$httpProvider.interceptors.push('xmlHttpInterceptor');
+	    })
+    	.factory('synonyms', function synonymsFactory($http, $q) {
 		
-    	var getSynonyms = function (word) {
+	    	var getSynonyms = function (word) {
 
-    		var deferred = $q.defer();
+	    		var deferred = $q.defer();
 
-    		var apiEndpoint = 'https://www.dictionaryapi.com/api/v1/references/thesaurus/xml/',
-    			keyString = '?key=' + apiKeys.thesaurus;
+	    		var apiEndpoint = 'https://www.dictionaryapi.com/api/v1/references/thesaurus/xml/',
+	    			keyString = '?key=' + apiKeys.thesaurus;
 
-    		var url = apiEndpoint + word + keyString;
+	    		var url = apiEndpoint + word + keyString;
 
-    		$http.get(url)
-    			.success(function(data) {
-    				//process response
-    				deferred.resolve(data);
-    			})
-    			.error(function(e) {
-    				deferred.reject(e);
-    			});
+	    		$http.get(url)
+	    			.success(function(data) {
+	    				console.log('getSynonyms response data: ', data);
+	    				//process response
+	    				deferred.resolve(data);
+	    			})
+	    			.error(function(e) {
+	    				console.log('getSynonyms response error: ', e);
+	    				deferred.reject(e);
+	    			});
 
-    		return deferred.promise;
-    	};
+	    		return deferred.promise;
+	    	};
 
-    	return {
-    		getSynonyms : getSynonyms
-    	};
+	    	return {
+	    		getSynonyms : getSynonyms
+	    	};
 
     });
 });
